520524cca339e9979493069ad6a391d572ca60f2
PiggySpeed <jlee14c@gmail.com>
2018-01-05 23:41:08 -0800

    promises

--- src/utils/promises.js
+++ src/utils/promises.js
@@ -1,12 +1,44 @@
 //'use strict';
 // ------------------------------------------
 
-var x = {
-  tree: 3,
-  bear: 56,
-  pig: 12,
-  cat: 67,
-  seal: 23123
-};
+// Basic promise demo
+function asyncOperation(fn) {
+  setTimeout(fn, 2000);
+}
 
+function countTo100() {
+  for (let i = 0; i <= 100; i++) {
+    console.log('counting ', i);
+  }
+  return true;
+}
 
+function bear() {
+  return new Promise(function(res, rej) {
+
+    console.log('starting promise');
+
+    asyncOperation(function() {
+      countTo100();
+      rej('noooooo');
+      console.log('hello');
+      // res('success2');
+    });
+
+    // if (countTo100()) {
+    //   res('success');
+    // } else {
+    //   rej('errrorrr!!!');
+    // }
+  });
+}
+
+function returnHi(val) {
+  console.log(val);
+  return 'hi';
+}
+
+bear()
+  .then(val => returnHi(val))
+  .then(val2 => console.log(val2))
+  .catch(err => console.log(err));
