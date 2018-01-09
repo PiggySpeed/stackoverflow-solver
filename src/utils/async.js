41910ea94acc5d6340a0e018ea1f6a1a621b20dc
PiggySpeed <jlee14c@gmail.com>
2018-01-08 22:50:48 -0800

    async demo

---
+++ src/utils/async.js
@@ -0,0 +1,28 @@
+
+function returnPromise(x) {
+  return new Promise(function(res, rej) {
+    setTimeout(function() {
+      const OPERATION_SUCCESSFUL = true;
+
+      if (OPERATION_SUCCESSFUL) {
+        // if async operation was successful
+        res("Operation successful: ", x);
+      } else {
+        // if async operation was not successful
+        rej('Error: async operation not successful');
+      }
+    }, 2000);
+  })
+}
+
+async function bear() {
+  // console.log('tree1');
+  // console.log(typeof (await returnPromise('hallo')));
+  const x = await returnPromise('hallo');
+  // console.log('it is ', typeof x);
+  return false;
+}
+
+bear()
+  .then(val => console.log(val))
+  .catch(err => console.log(err));
