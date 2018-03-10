6d4896ba68f1370c2f7f36e599b42e15499ad052
John Lee <jlee14c@hotmail.com>
2018-03-09 19:11:00 -0800

    updates

--- src-node-test/index.js
+++ src-node-test/index.js
@@ -1,1 +1,14 @@
-console.log("included the index file");
+(function() {
+    let $smsForm = document.getElementById('sms-form');
+    let $submitBtn = document.getElementById('post');
+
+    $smsForm.onSubmit = () => {
+        e.preventDefault();
+        let confirmation = window.confirm("Send this text message");
+
+        if (confirmation) {
+            $submitBtn.disabled = true;
+            $smsForm.submit();
+        }
+    }
+})();
