5c4a4138b2a26c07f330a2d3ccfb7948f84ee237
John Lee <jlee14c@hotmail.com>
2018-03-09 19:23:10 -0800

    updates

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const https = require('https');
 
+const accountSid = 'AC2e57bdd1b62b43e451c2d1c3b769d10b';
+const authToken = '893a6ad2d6765f78e87c48d8f77ff4fa';
+const client = require('twilio')(accountSid, authToken);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('src-node-test'));
 
 app.get('/', (req, res) => {
