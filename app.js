4bb318c5c5e5464ba115ac824339aea1f7dadb88
John Lee <jlee14c@hotmail.com>
2018-03-09 19:28:52 -0800

    updates

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('src-node-test'));
 
+app.post('/send-sms', (req, res) => {
+    let phoneNumber = req.body.phoneNumber;
+    let message = req.body.message;
+
+    client.messages.create({
+        to: `+1${phoneNumber}`,
+        from: '+1778615337',
+        body: message
+    },
+    (err, message) => {
+        if (err) {
+            req.status(400).send("Error has occurred, text message has not been sent");
+        } else {
+            console.log(`Successfully sent text: ${message.sid}`);
+            req.status(200).send("Message has been successfully sent");
+        }
+    })
+});
+
 app.get('/', (req, res) => {
     res.send("index.html");
 });
